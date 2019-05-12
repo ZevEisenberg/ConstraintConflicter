2baf2a0254e1a09889f5a3658faba8e383224a6c
Zev Eisenberg <zeisenberg@rightpoint.com>
2019-05-12 17:42:38 -0400

    Initial commit.

---
+++ ConstraintConflicter/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ConstraintConflicter
+//
+//  Created by Zev Eisenberg on 5/12/19.
+//  Copyright © 2019 Zev Eisenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myView = UIView()
+        myView.accessibilityIdentifier = "My cool view"
+
+        view.addSubview(myView)
+        myView.translatesAutoresizingMaskIntoConstraints = false
+
+        #warning("These two constraints will conflict")
+        myView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        myView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+
+        myView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50)
+        myView.heightAnchor.constraint(equalToConstant: 40)
+    }
+
+}
