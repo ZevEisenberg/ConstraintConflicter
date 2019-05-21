febfe1a3f170c58706fcfd5fd8e677a176bb59d3
Zev Eisenberg <zeisenberg@rightpoint.com>
2019-05-21 11:20:41 -0400

    Add comment.

--- ConstraintConflicter/ViewController.swift
+++ ConstraintConflicter/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         let myView = UIView()
+        // Setting the accessibilityIdentifier will give this
+        // view a label in Auto Layout debug output.
         myView.accessibilityIdentifier = "My cool view"
 
         view.addSubview(myView)
